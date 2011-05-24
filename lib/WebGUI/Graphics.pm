99cabc35a2d81a6d54915d1e696a2acb71b5de99
kthakore <thakore.kartik@gmail.com>
2011-05-23 21:10:24 -0400

    Changed the style, ran perlcritic

--- lib/WebGUI/Graphics.pm
+++ lib/WebGUI/Graphics.pm
@@ -7,133 +7,130 @@ use warnings;
 use Carp qw( croak );
 use Image::Magick;
 use GD;
-use GD::Thumbnail; 
+use GD::Thumbnail;
 
+sub new {
+    my $class = shift;
 
-sub new 
-{
-	my $class = shift;
-	
-	my $wrap = bless {@_}, $class;
-	$wrap->{core} = Image::Magick->new( @_ );
+    my $wrap = bless {@_}, $class;
+    $wrap->{core} = Image::Magick->new(@_);
 
-	return $wrap; 
-	# return $warp->{core}; 
+    return $wrap;
+
+    # return $warp->{core};
 
 }
 
-sub Set
-{
-	my $self = shift; return $self->{core}->Set( @_ );
+sub Set {
+    my $self = shift;
+    return $self->{core}->Set(@_);
 }
 
-sub ReadImage
-{
-	my $self = shift; return $self->{core}->ReadImage( @_ );
+sub ReadImage {
+    my $self = shift;
+    return $self->{core}->ReadImage(@_);
 }
 
-sub AddNoise
-{
-	my $self = shift; return $self->{core}->AddNoise( @_ );
+sub AddNoise {
+    my $self = shift;
+    return $self->{core}->AddNoise(@_);
 }
 
-sub Draw
-{
-	my $self = shift; return $self->{core}->Draw( @_ );
+sub Draw {
+    my $self = shift;
+    return $self->{core}->Draw(@_);
 }
 
-sub Blur
-{
-	my $self = shift; return $self->{core}->Blur( @_ );
+sub Blur {
+    my $self = shift;
+    return $self->{core}->Blur(@_);
 }
 
-sub Border
-{
-	my $self = shift; return $self->{core}->Border( @_ );
+sub Border {
+    my $self = shift;
+    return $self->{core}->Border(@_);
 }
 
-sub Write
-{
-	my $self = shift; return $self->{core}->Write( @_ );
+sub Write {
+    my $self = shift;
+    return $self->{core}->Write(@_);
 }
 
-sub Get 
-{
-	my $self = shift; return $self->{core}->Get( @_ );
+sub Get {
+    my $self = shift;
+    return $self->{core}->Get(@_);
 }
 
-sub Scale 
-{
-	my $self = shift; return $self->{core}->Scale( @_ );
+sub Scale {
+    my $self = shift;
+    return $self->{core}->Scale(@_);
 }
 
-sub Sharpen
-{
-	my $self = shift; return $self->{core}->Sharpen( @_ );
+sub Sharpen {
+    my $self = shift;
+    return $self->{core}->Sharpen(@_);
 }
 
-sub Read
-{
-	my $self = shift; return $self->{core}->Read( @_ );
+sub Read {
+    my $self = shift;
+    return $self->{core}->Read(@_);
 }
 
-
-sub Crop
-{
-	my $self = shift; return $self->{core}->Crop( @_ );
+sub Crop {
+    my $self = shift;
+    return $self->{core}->Crop(@_);
 }
 
-sub Rotate
-{
-	my $self = shift; return $self->{core}->Rotate( @_ );
+sub Rotate {
+    my $self = shift;
+    return $self->{core}->Rotate(@_);
 }
 
-sub Resize
-{
-	my $self = shift; return $self->{core}->Resize( @_ );
+sub Resize {
+    my $self = shift;
+    return $self->{core}->Resize(@_);
 }
 
-sub Annotate
-{
-	my $self = shift; return $self->{core}->Annotate( @_ );
+sub Annotate {
+    my $self = shift;
+    return $self->{core}->Annotate(@_);
 }
 
 sub genThumbnailRefactor {
 
-	my $filename = shift;
-	my $thumbnailSize = shift; 
-	my $thumbname = shift; 
+    my $filename      = shift;
+    my $thumbnailSize = shift;
+    my $thumbname     = shift;
 
-	# Open the image or return the error message 
-	my $image = GD::Image->new( $filename ) || return "Couldn't read image for thumbnail creation: $!"; 
-	my ($x, $y) = $image->getBounds();
-	my $thumb = GD::Thumbnail->new || return $!;
-	my $raw = $image->gd; 
-	my $n = $thumbnailSize;
-	    if ($x > $n || $y > $n) {
-                my $r = $x>$y ? $x / $n : $y / $n;
-                $x /= $r;
-                $y /= $r;
-                if($x < 1) { $x = 1 } # Dimentions < 1 cause Scale to fail
-                if($y < 1) { $y = 1 }
- 				$raw   = $thumb->create( $filename, $x, $y);
-			}
-    
-	my $error = write_gd( $thumbname, $raw );
-	return $error; 	
-}
+    # Open the image or return the error message
+    my $image = GD::Image->new($filename) || return "Couldn't read image for thumbnail creation: $!";
+    my ( $x, $y ) = $image->getBounds();
+    my $thumb = GD::Thumbnail->new || return $!;
+    my $raw   = $image->gd;
+    my $n     = $thumbnailSize;
+    if ( $x > $n || $y > $n ) {
+        my $r = $x > $y ? $x / $n : $y / $n;
+        $x /= $r;
+        $y /= $r;
+        if ( $x < 1 ) { $x = 1 }    # Dimentions < 1 cause Scale to fail
+        if ( $y < 1 ) { $y = 1 }
+        $raw = $thumb->create( $filename, $x, $y, '' );
+    }
 
-sub write_gd
-{
-	my $filename = shift;
-	my $raw = shift; 
-   
-   open    IMG, ">$filename" or return $!;
-   binmode IMG;
-   print   IMG $raw;
-   close   IMG;
+    my $error = write_gd( $thumbname, $raw );
+    return $error;
+} ## end sub genThumbnailRefactor
 
-	return 0;
+sub write_gd {
+    my $filename = shift;
+    my $raw      = shift;
+
+    open my $IMG, ">", "$filename" or return $!;
+    binmode $IMG;
+    print $IMG $raw;
+    close $IMG;
+
+    return 0;
 }
 
 1;
