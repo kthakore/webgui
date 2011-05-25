8ac7a3884b808d9141755002715690750ce72383
kthakore <thakore.kartik@gmail.com>
2011-05-25 18:31:21 -0400

    Can't figure out the test erros

--- lib/WebGUI/Storage.pm
+++ lib/WebGUI/Storage.pm
@@ -27,8 +27,6 @@ use Path::Class::Dir;
 use Storable ();
 use WebGUI::Paths;
 use JSON ();
-use Try::Tiny;
-use GD;
 
 
 =head1 NAME
@@ -1275,22 +1273,7 @@ sub getSizeInPixels {
         return $image->Get('width','height');
 
 =cut 
-
-	my $error; 
-	my $image; 
-	try
-	{
-		$image = GD::Image->new($filename) or die  "Couldn't read image for size reading $!";
-		return	$image->getBounds();
-
-	}
-	catch
-	{
-		$self->session->log($_);
-		return 0;
-	
-	};
-
+	return WebGUI::Graphics::getSizeRefactor($self, $filename);
 }
 
 
